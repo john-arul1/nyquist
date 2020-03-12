6d3757f05093f51dc6d9d51119b89263ee7ed12f
john-arul1 <34236264+john-arul1@users.noreply.github.com>
2020-03-12 18:23:51 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 
-K=complex(1.0E2,0)
+N0=complex(1.0E2,0)
 lam=complex(0.1,0)
 beta=complex(3.0E-3,0)
 L=complex(1.0E-6,0)
@@ -8,6 +8,8 @@ alpha=complex(1.0E-5,0)
 
 def CF(s):
     w=complex(0,0)
+    w=100.0/(s+1)/(s/10+1)
     #w=240.0/(s-4)/(s+9)/(s+5)
-    w =K/(s*L+s*beta/(lam+s)-alpha*K)
+    #w =N0/(s*L+s*beta/(lam+s)-alpha*K)   # form1
+    w =1.0/L*(lam+s)/(s*(s+lam+beta/L))   # form2  (dn/N0)/dr
     return w
